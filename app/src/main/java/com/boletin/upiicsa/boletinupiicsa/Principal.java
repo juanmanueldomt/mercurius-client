935fb6edecc7b206a5dba46fcaa0d46eaa951289
Juan Manuel Dominguez Tlapale <cronoz.v@gmail.com>
2017-03-14 15:16:23 -0600

    Version 0.0.1

--- app/src/main/java/com/boletin/upiicsa/boletinupiicsa/Principal.java
+++ app/src/main/java/com/boletin/upiicsa/boletinupiicsa/Principal.java
@@ -3,6 +3,7 @@ package com.boletin.upiicsa.boletinupiicsa;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Base64;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,10 +17,12 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.io.InputStream;
+
 
 public class Principal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,11 +47,76 @@ public class Principal extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("http://192.168.1.2/boletin_upiicsa");
+
+
+        myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                // Inject CSS when page is done loading
+                injectCSS();
+                super.onPageFinished(view, url);
+            }
+        });
+
+        //String style= "<head><link rel=\"stylesheet\" href=\"http://10.42.0.1/mercurius/resources/bootstrap-3.3.5-dist/css/mercurius-client.css\"></head>";
+        //myWebView.loadData(style,"text/html","utf-8");
+
+        myWebView.loadUrl("http://mercurius-swfipn.rhcloud.com/");
+
+
+
+       /* webView = new WebView(this);
+        setContentView(webView);
+
+        // Enable Javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // Add a WebViewClient
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+
+                // Inject CSS when page is done loading
+                injectCSS();
+                super.onPageFinished(view, url);
+            }
+        });
+
+        // Load a webpage
+        webView.loadUrl("http://10.42.0.1/mercurius");
+    */
+    }
+
+    // Inject CSS method: read style.css from assets folder
+// Append stylesheet to document head
+    private void injectCSS() {
+        try {
+            InputStream inputStream = getAssets().open("style.css");
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            inputStream.close();
+            String encoded = Base64.encodeToString(buffer, Base64.NO_WRAP);
+            myWebView.loadUrl("javascript:(function() {" +
+                    "var parent = document.getElementsByTagName('head').item(0);" +
+                    "var style = document.createElement('style');" +
+                    "style.type = 'text/css';" +
+                    // Tell the browser to BASE64-decode the string into your script !!!
+                    "style.innerHTML = window.atob('" + encoded + "');" +
+                    "parent.appendChild(style)" +
+                    "})()");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+
     }
 
     @Override
